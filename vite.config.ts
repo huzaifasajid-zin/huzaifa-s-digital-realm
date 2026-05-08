b7ef6b9b70f6ee395b9bd7264f35de6c2a94b942
Huzaifa Sajid <zin.huzaifa.sajid@gmail.com>
2026-05-08 14:46:00 +0500

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    react(),
     netlify(),
     tsconfigPaths(),
   ],
